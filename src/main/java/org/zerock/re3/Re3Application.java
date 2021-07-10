5496fc2d53440946cc67f087416ce753f66465a9
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-07-10 17:58:03 +0900

    Initial commit

---
+++ src/main/java/org/zerock/re3/Re3Application.java
@@ -0,0 +1,13 @@
+package org.zerock.re3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Re3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Re3Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/re3/Re3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.re3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Re3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
