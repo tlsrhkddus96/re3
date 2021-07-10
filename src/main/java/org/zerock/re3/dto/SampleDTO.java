51f5ee163ae66e0b5ae17fe3399326af61108c10
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-07-10 20:49:03 +0900

    ThymeLeaf

---
+++ src/main/java/org/zerock/re3/dto/SampleDTO.java
@@ -0,0 +1,21 @@
+package org.zerock.re3.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+
+    private String first;
+
+    private String last;
+
+    private LocalDateTime regTime;
+
+
+}
